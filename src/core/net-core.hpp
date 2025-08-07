e14189bedb0565f68a8d470acbe27f9dab135f3b
Ashkan-sm <ashkan.sedighi.moghadam@gmail.com>
2025-08-07 10:28:12 +0330

    refactor(ashkan-tool): change project structure
    changed project structure

---
+++ src/core/net-core.hpp
@@ -0,0 +1,82 @@
+//
+// Created by dev on 6/5/25.
+//
+
+#ifndef ASHKANTOOL_NET_CORE_HPP
+#define ASHKANTOOL_NET_CORE_HPP
+
+#include <iostream>
+#include "MacAddress.h"
+#include "IpAddress.h"
+#include "Logger.h"
+#include "PcapPlusPlusVersion.h"
+#include "PcapLiveDeviceList.h"
+#include "PcapLiveDevice.h"
+#include "NetworkUtils.h"
+#include <getopt.h>
+#include "SystemUtils.h"
+#include "EthLayer.h"
+#include "Packet.h"
+#include "utils/logger.hpp"
+#include "utils/net-utils.hpp"
+#include "task/task.hpp"
+#include "task/tasks/arp-poisoning-task.hpp"
+#include "task/tasks/arp-poison-detection-task.hpp"
+#include "task/tasks/vlan-hopping-task.hpp"
+#include "task/tasks/dtp-negotiation.hpp"
+#include "task/tasks/mitm-packet-forwarding.hpp"
+#include "task/tasks/dtp-domain-extraction-task.hpp"
+#include "task/tasks/wifi-ap-scanning-task.hpp"
+#include "task/tasks/wifi-host-scanning-task.hpp"
+#include "task/tasks/deauth-packet-sending.hpp"
+#include "task/tasks/wpa-2-hand-shake-capture-task.hpp"
+#include "task/tasks/wifi-password-cracking-task.hpp"
+
+#include "core/data-holders/wifi.hpp"
+
+#include <map>
+
+#include "core/packet-receiver/packet-receiver.hpp"
+#include "capture-wrapper.hpp"
+namespace ashk {
+    class Net_core {
+    public:
+        friend class ModelInterface;
+        Net_core();
+
+    private:
+        pcpp::MacAddress arp(pcpp::IPv4Address ip);
+        void send_arp_req(pcpp::IPv4Address iface_ip,pcpp::IPv4Address ip);
+        int discover_interface();
+        std::vector<pcpp::PcapLiveDevice *> discover_interfaces();
+
+        pcpp::IPv4Address interface_ip();
+        std::string interface_name();
+
+        void start_arp_poisoning(pcpp::IPv4Address iface_ip, pcpp::IPv4Address vic_src_ip, pcpp::IPv4Address vic_dst_ip,
+                                 pcpp::IPv4Address forward_to_ip);
+        void start_arp_poison_detection(pcpp::IPv4Address iface_ip);
+        void start_mitm_forwarding(pcpp::IPv4Address iface_ip,  pcpp::IPv4Address victim_ip, pcpp::IPv4Address gateway_ip,pcpp::MacAddress victim_mac, pcpp::MacAddress gateway_mac) ;
+        void start_vlan_hopping(pcpp::IPv4Address iface_ip,int outer_id,int inner_id);
+        void start_dtp_negotiation(pcpp::IPv4Address iface_ip,const std::string& domain_name);
+        void start_dtp_domain_extraction(pcpp::IPv4Address iface_ip,char *buffer);
+        void start_detecting_wifi_aps(std::string iface_name_or_ip,std::vector<WifiAp> &ap_list);
+        void start_detecting_wifi_hosts(const std::string& iface_ip_name_str, std::vector<std::shared_ptr<WifiHost>>&host_list);
+        void start_sending_deauth_packets(const std::string& iface_ip_name_str,WifiAp* wifi_ap, std::vector<std::shared_ptr<WifiHost>>&host_list);
+        void start_password_cracking(const std::string& iface_ip_name_str,std::shared_ptr<HandShakeData> handshake_data);
+        void start_wpa2_handshake_capturing(const std::string& iface_ip_name_str,std::shared_ptr<HandShakeData> handshake_data);
+
+        void add_logger_method(const std::function<void(const std::string &)> &method);
+        void end_task(int id);
+
+        std::vector<int> get_running_tasks();
+
+        pcpp::PcapLiveDevice *dev_ = nullptr;
+        utils::Logger &logger_ = utils::Logger::getInstance();
+        CaptureWrapper &capture_wrapper = CaptureWrapper::getInstance();
+        std::map<int, std::shared_ptr<Task>> tasks;
+        int last_added_task_id = 0;
+    };
+}
+
+#endif //ASHKANTOOL_NET_CORE_HPP
---
+++ src/ui/windows/sub-window.hpp
@@ -0,0 +1,61 @@
+//
+// Created by dev on 6/4/25.
+//
+
+#ifndef ASHKANTOOL_SUB_WINDOW_HPP
+#define ASHKANTOOL_SUB_WINDOW_HPP
+#include "imgui-headers.hpp"
+#include "config.h"
+#include "../../core/interface.hpp"
+#include <EthDot3Layer.h>
+#include "core/data-holders/wifi.hpp"
+#include "utils/logger.hpp"
+class ISubWindow {
+public:
+    explicit ISubWindow(ashk::ModelInterface *core);
+    virtual void draw() = 0;
+protected:
+    ashk::ModelInterface *core_;
+    void draw_base(const std::string &name);
+    pcpp::PcapLiveDevice * draw_interface_widget();
+private:
+};
+class DefaultWindow :public ISubWindow{
+public:
+    explicit DefaultWindow(ashk::ModelInterface *core);
+    void draw() override;
+};
+
+class ArpSpoofWindow :public ISubWindow{
+public:
+    explicit ArpSpoofWindow(ashk::ModelInterface *core);
+    void draw() override;
+};
+
+class ArpPoisonDetectionWindow :public ISubWindow{
+public:
+    explicit ArpPoisonDetectionWindow(ashk::ModelInterface *core);
+    void draw() override;
+};
+class SendArpRequestWindow :public ISubWindow{
+public:
+    explicit SendArpRequestWindow(ashk::ModelInterface *core);
+    void draw() override;
+};
+class VlanHoppingWindow  :public ISubWindow{
+public:
+    explicit VlanHoppingWindow(ashk::ModelInterface *core);
+    void draw() override;
+};
+class MITMWindow  :public ISubWindow{
+public:
+    explicit MITMWindow(ashk::ModelInterface *core);
+    void draw() override;
+};
+class WIFIAttackWindow  : public ISubWindow{
+public:
+    explicit WIFIAttackWindow(ashk::ModelInterface *core);
+    void draw() override;
+};
+
+#endif //ASHKANTOOL_SUB_WINDOW_HPP
