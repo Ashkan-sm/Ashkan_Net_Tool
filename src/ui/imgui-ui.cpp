54c9b34ac56ba5015a53ecc507a7f662a1ba600d
Ashkan-sm <ashkan.sedighi.moghadam@gmail.com>
2025-08-07 15:58:48 +0330

    merge(imgui): update imgui

--- src/ui/imgui-ui.cpp
+++ src/ui/imgui-ui.cpp
@@ -89,8 +89,6 @@ int ImguiUi::Exec() {
   //IM_ASSERT(font != nullptr);
 
   // Our state
-  bool show_demo_window = true;
-  bool show_another_window = false;
   ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
   // Main loop
